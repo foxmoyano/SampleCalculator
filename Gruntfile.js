6db562193427deb5310f0908cf5517ccc8bc901d
Luis Moyano <lmoyano@pragma.cl>
2023-03-30 21:27:02 -0300

    Se agrega uglify-js y se elimina plugin grunt-contrib-uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,11 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
-      clean: ['dist', 'build'],
+      clean: {
+        init: ['dist', 'build'],
+        tmp: ['app.full.min.js'],
+        build: ['build']
+      },
       dom_munger:{
         read: {
           options: {
@@ -29,7 +33,7 @@ module.exports = function(grunt) {
         }
       },  
       jshint: {
-        all: ['js/*.js'],
+        all: ['js/app.js'],
         options: {
           'esversion': 6,
         }        
@@ -51,35 +55,33 @@ module.exports = function(grunt) {
           dest: `dist/style.full.${nombreDist}.min.css`
         }
       },
-      uglify: {
-        /*js1: {
-          src: 'js/app.js',
-          dest: 'build/app.min.js'
-        },*/
-        js2: {
-          src: 'bower_components/angular/angular.js',
-          dest: 'build/angular.min.js'
+      copy: {
+        main: {
+          files: [
+            {
+              src: 'app.full.min.js',
+              dest: `dist/app.full.${nombreDist}.min.js`
+            }    
+          ]
         }
       },
-      concat: {
-        options: {
-          separator: ';'
-        },
-        dist: {
-          src: ['build/angular.min.js', 'app.min.js'],
+      uglify: {
+        js: {
+          src: 'build/app.full.min.js',
           dest: `dist/app.full.${nombreDist}.min.js`
         }
-      },
+      }
     });
   
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-dom-munger');    
   
-    grunt.registerTask('default', ['jshint', 'clean', 'cssmin', 'uglify', 'concat', 'dom_munger', 'htmlmin']);    
+    grunt.registerTask('default', ['clean:init', 'jshint', 'copy', 'cssmin', 'dom_munger', 'htmlmin', 'clean:build', 'clean:tmp']);
   };
+  
   
--- README.md
+++ README.md
@@ -4,30 +4,40 @@ Simple calculadora AngularJS y minificada con Grunt.
 
 ## Instalación
 
-Descargar el ejemplo desde git
+Primero instalemos grunt
+```
+npm i -g grunt-cli
+```
+
+Luego instalemos **uglify-js**
+```
+npm i -g uglify-js
+```
+
+Ahora descarguemos el ejemplo desde git
 
 ```
 git clone https://github.com/foxmoyano/SampleCalculator.git
 ```
 
-Entrar a la carpeta principal e instalar las dependencias
+Entrar a la carpeta principal e instalar las dependencias del ejemplo
 ```
-npm install
-```
-
-
-Basicamente se utilizaron 2 carpetas:
-
-**build**: carpeta que contiene los archivos temporales creados por las tareas de grunt.
-**dist**: carpeta donde quedan los archivos finales minificados.
-
-Para minificar los archivos js se utilizo **uglifyjs** la cual no es parte de las tareas del grunt.
-
-Para usar **uglifyjs** se ejecutó lo siguiente, antes de usar grunt:
-
-```
-uglifyjs js/app.js -o app.min.js
+npm i
 ```
 
+## Ejecución
+
+La ejecución esta dividida en 2 partes, primero hacemos el empaquetado de los js y minificamos con **uglifyjs**
+
+```
+uglifyjs js/angular.js js/app.js -o app.full.min.js
+```
+
+La segunda parte se realiza con grunt, para lo cual solamente lo ejecutamos
+```
+grunt
+```
+
+Esto genera la carpeta dist con el html, css y js minificados.
 
 
